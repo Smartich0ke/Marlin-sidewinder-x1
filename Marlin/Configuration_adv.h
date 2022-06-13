83c110c077e9f9ea6a3c01b0f6f1a91e7f7f5c67
Nikolai Patrick <nikolaipatricknp@gmail.com>
2022-06-13 14:47:46 +0930

    change case light pin

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -666,7 +666,7 @@
  */
 #define CASE_LIGHT_ENABLE
 #if ENABLED(CASE_LIGHT_ENABLE)
-  #define CASE_LIGHT_PIN 5                  // Override the default pin if needed
+  #define CASE_LIGHT_PIN 21                  // Override the default pin if needed
   #define INVERT_CASE_LIGHT true             // Set true if Case Light is ON when pin is LOW
   #define CASE_LIGHT_DEFAULT_ON true          // Set default power-up state on
   #define CASE_LIGHT_DEFAULT_BRIGHTNESS 255   // Set default power-up brightness (0-255, requires PWM pin)
